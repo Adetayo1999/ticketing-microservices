fddc0c908f4cbc52d6d4963a0bb6cc280337828c
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-02-02 08:33:15 +0100

    created the authentication service ✨

---
+++ auth/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleswares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} ✨🚀`);
+});
