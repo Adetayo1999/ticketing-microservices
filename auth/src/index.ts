44e82d0e726bab488f4883abc65a22864b40598e
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-09-10 08:30:18 +0100

    updated the auth microservice

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,15 @@
+import { CONFIG_KEYS } from "./common/config";
+import connectDB from "./database";
 import { createServer } from "./server";
 
 async function main() {
+  if (Object.values(CONFIG_KEYS).some((env) => env === undefined)) {
+    throw new Error("Invalid Configuration");
+  }
   const PORT = process.env.PORT || 3000;
+
+  await connectDB();
+
   const server = await createServer();
 
   server.listen(PORT, () => {
--- auth/src/server/index.ts
+++ auth/src/server/index.ts
@@ -5,8 +5,6 @@ import logger from "morgan";
 import cookieSession from "cookie-session";
 import { getAllRoutes } from "../routes";
 import { errorHandler } from "../middlewares/error-handler";
-import connectDB from "../database";
-import { CONFIG_KEYS } from "../common/config";
 import { BadRequestError } from "../errors/bad-request-error";
 
 export const createServer = async () => {
@@ -32,12 +30,6 @@ export const createServer = async () => {
   });
 
   app.use(errorHandler);
-
-  if (Object.values(CONFIG_KEYS).some((env) => env === undefined)) {
-    throw new Error("Invalid Configuration");
-  }
-
-  await connectDB();
 
   return app;
 };
