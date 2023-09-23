b985762f3e24c325db3a3fe4550824954f7e0c1d
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-09-23 22:29:43 +0100

    created the client and infra

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl text-center text-white">About Page</h1>
+    </div>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-4xl text-center text-white">
+        Ticketing Microservices
+      </h1>
+    </main>
+  );
+}
